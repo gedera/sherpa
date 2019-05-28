e55c0bb0446af59a76bb35c95fa2326155462b58
ga6ix <gab.edera@gmail.com>
2019-05-28 17:48:36 -0300

    fuck

---
+++ bin/docker-cmd-server.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+server_pid="/tmp/rails.pid"
+
+finish() {
+  if [ -f $server_pid ]; then
+    pid=$(cat $server_pid)
+
+    if [ $pid -ne 0 ]; then
+      kill -SIGINT $pid
+      wait $pid
+    fi
+  fi
+  exit 143
+}
+trap finish SIGKILL SIGTERM SIGHUP SIGINT EXIT
+
+# Fresh install
+rails db:create
+rails db:migrate
+
+rails db:seed
+
+cp -Rf public/assets/* public/assets_mounted/
+
+bundle exec rails server &
+wait $!
